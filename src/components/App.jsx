488eb4a3af35aa73a43522aefdaf169ccb2a7fcc
chun-eric <eric.chun88800888@gmail.com>
2023-01-13 15:39:15 +0900

    Basic Folder Structure and Dependency Config without ESLINT

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,30 @@
 
 import React from 'react'
+import { CssBaseline } from '@mui/material'
+import { Routes, Route } from 'react-router-dom'
+import { Actors, MovieInformation, Movies, Navbar, Profile } from '.'
+
+
 
 const App = () => {
+
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <div style={{ display: 'flex', height: '100%' }}>
+        <CssBaseline />
+        <Navbar />
+        <main style={{ flexGrow: '1', padding: '2em', width: '100%' }}>
+          <div style={{ height: '70px' }} />
+          <Routes>
+            <Route exact path="/" element={<Movies />} />
+            <Route path="/movie/:id" element={<MovieInformation />} />
+            <Route path="/actor/:id" element={<Actors />} />
+            <Route path="/profile/:id" element={<Profile />} />
+          </Routes>
+        </main>
+      </div >
+    </>
+  );
+};
 
 export default App
